55fde8066f69917ceec0eab00530bfa577b006a4
draj1112 <jprincessdanica@gmail.com>
2016-03-18 18:14:42 +0800

    JSON Parsing2

--- app/src/main/java/com/example/android/sunshine/activities/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/activities/ForecastFragment.java
@@ -241,6 +241,16 @@ public class ForecastFragment extends Fragment {
                 }
                 return null;
             }
+
+            @Override
+            protected void onPostExecute(String[] result) {
+                if (result != null) {
+                    mForecastAdapter.clear();
+                    for(String dayForecastStr : result) {
+                        mForecastAdapter.add(dayForecastStr);
+                    }
+                }
+            }
         }
 }
 
