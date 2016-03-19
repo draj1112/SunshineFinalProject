20697664060e86cc68e5b47a45a9708e0086437b
draj1112 <jprincessdanica@gmail.com>
2016-03-19 09:07:23 +0800

    URI Builder Weather with Location

--- app/src/main/java/com/example/android/sunshine/data/FetchWeatherTask.java
+++ app/src/main/java/com/example/android/sunshine/data/FetchWeatherTask.java
@@ -25,6 +25,7 @@ import android.text.format.Time;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 
+import com.example.android.sunshine.BuildConfig;
 import com.example.android.sunshine.R;
 
 import org.json.JSONArray;
@@ -330,12 +331,14 @@ public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
             final String FORMAT_PARAM = "mode";
             final String UNITS_PARAM = "units";
             final String DAYS_PARAM = "cnt";
+            final String APPID_PARAM = "APPID";
 
             Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
                     .appendQueryParameter(QUERY_PARAM, params[0])
                     .appendQueryParameter(FORMAT_PARAM, format)
                     .appendQueryParameter(UNITS_PARAM, units)
                     .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                    .appendQueryParameter(APPID_PARAM, BuildConfig.OPEN_WEATHER_MAP_API_KEY)
                     .build();
 
             URL url = new URL(builtUri.toString());
